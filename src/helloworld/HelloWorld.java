d87ff08b648366fe236b95277e27f370a84d621d
bit <bit@DESKTOP-2GQ31VL>
2020-05-08 20:42:49 +0900

    init commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,34 @@
+package helloworld;
+
+/*
+Hello World 클래스 정의
+작성자 안대혁
+작성일 2020.05.07
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		System.out.println("Hello Wolrd2");
+		
+		
+		
+		// 값(Vlaue)
+		int number1 = 10;
+		int number2 = 5 + 5;
+		int number3 = ten();
+
+		System.out.println(number1 + ":" + number2 + ":" + number3);
+	}
+	
+	public static int ten() {
+		return 10;
+	}
+}
